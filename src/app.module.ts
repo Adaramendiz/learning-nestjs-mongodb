c2a3412c8d447198f5c20e6d83677bfe6c6dcb9b
Andres Aramendiz <adaramendiz@gmail.com>
2024-03-28 13:27:56 -0500

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/products'),
     ProductsModule,
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
